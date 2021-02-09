cdeca3c54b0f8570ed6ff5c295fd0bbe3c2e072c
ayush.sha <ayush.sha@internal.media.net>
2021-02-09 20:03:26 +0530

    home page with all posts with trimed body and read more

--- src/components/IndividualPosts.js
+++ src/components/IndividualPosts.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import "../css/IndividualPostCSS.css";
 import Card from "react-bootstrap/Card";
+import { Link } from 'react-router-dom';
+
+const trimLength = 61;
 
 class IndividualPosts extends Component {
 
-    
+
+    getTrimedText = () => {
+        let displayString = "";
+        let body = this.props.post.body;
+        displayString = body.substring(0,trimLength);
+        if(this.compareDisplayLength(displayString.length, body.length)){
+            displayString += `...`;
+        }
+        return displayString;
+    }
+
+    compareDisplayLength(displayLength, bodyLength){
+        if(displayLength !== bodyLength){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 
     render() {
-    
+        let displayString = this.getTrimedText();
+        let body = this.props.post.body;
+        let url = `/${this.props.post.id}`;
         return (    
             <div className="individual-post-container">
                 <Card className="individual-post-card">
@@ -16,7 +39,14 @@ class IndividualPosts extends Component {
                     >{this.props.post.title}</Card.Title>
                     <Card.Body
                         className="individual-post-body"
-                    >{this.props.post.body}</Card.Body>
+                    >{displayString}{
+                        this.compareDisplayLength(displayString.length,body.length) ? (
+                        <Link
+                            to={url}
+                            className="read-more"
+                        >Read More</Link>
+                        ) : null
+                    }</Card.Body>
                 </Card>
                 
             </div>
